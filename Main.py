d700a87924bed6a3b2563e79f0c4a04f9c1cd094
Administrator <516545865@qq.com>
2018-03-28 09:16:34 +0800

    modify Main.py.

--- Main.py
+++ Main.py
@@ -6,11 +6,9 @@ def sub01():
     for i in range(10):
         print('Hello World!')
 
-
-
 def sub02():
-    print(os.cwd())
+    print(os.getcwd())
         
     
 if __name__ == '__main__':
-    sub01()
+    sub02()
