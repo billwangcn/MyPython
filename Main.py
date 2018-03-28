ecd097ade2824d25198c262b93bca80610773901
Bill Wang <wangbinpq@163.com>
2018-03-28 09:16:38 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
 #
 
+import os
+
 def sub01():
     for i in range(10):
         print('Hello World!')
 
 
+
+def sub02():
+    print(os.cwd())
+        
+    
 if __name__ == '__main__':
     sub01()
